f9c6f233e736a58922179b53300d727552c25371
MReeds <mattlreeds@gmail.com>
2020-05-05 11:37:15 -0500

    Added library details

--- libraryproject/libraryapp/urls.py
+++ libraryproject/libraryapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from django.urls import path
 from .views import *
 
 app_name = "libraryapp"
@@ -7,8 +8,10 @@ urlpatterns = [
     path('', home, name='home'),
     path('books/', book_list, name='books'),
     path('book/form', book_form, name='book_form'),
+    path('books/<int:book_id>/', book_details, name='book'),
     path('librarians/', list_librarians, name='librarians'),
     path('libraries/', list_libraries, name="libraries"),
+    path('libraries/<int:library_id>/', library_details, name='library'),
     path('library/form', library_form, name='library_form'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
