82cea908780b99a4c58691b1ce57a2f7034eb194
MReeds <mattlreeds@gmail.com>
2020-05-06 11:03:06 -0500

    Completed ch 8 editing a book

--- libraryproject/libraryapp/views/__init__.py
+++ libraryproject/libraryapp/views/__init__.py
@@ -4,7 +4,7 @@ from .libraries.form import library_form
 from .libraries.details import library_details
 from .librarians.details import librarian_details
 from .books.list import book_list
-from .books.form import book_form
+from .books.form import book_form, book_edit_form
 from .books.details import book_details
 from .auth.logout import logout_user
 from .home import home
--- libraryproject/libraryapp/views/books/form.py
+++ libraryproject/libraryapp/views/books/form.py
@@ -2,6 +2,7 @@ import sqlite3
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from libraryapp.models import Book
+from .details import get_book
 from libraryapp.models import Library
 from libraryapp.models import model_factory
 from ..connection import Connection
@@ -28,6 +29,21 @@ def book_form(request):
         libraries = get_libraries()
         template = 'books/forms.html'
         context = {
+            'all_libraries': libraries
+        }
+
+        return render(request, template, context)
+    
+@login_required
+def book_edit_form(request, book_id):
+
+    if request.method == 'GET':
+        book = get_book(book_id)
+        libraries = get_libraries()
+
+        template = 'books/forms.html'
+        context = {
+            'book': book,
             'all_libraries': libraries
         }
 
